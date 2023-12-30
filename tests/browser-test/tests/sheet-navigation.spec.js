00db28f1c41910f78c6b2ec3c5bcdc40090e8381
Dominik <sauterdominik1211@gmail.com>
2023-12-30 16:00:50 +0100

    CHanged Playwright Test Config

--- tests/browser-test/tests/sheet-navigation.spec.js
+++ tests/browser-test/tests/sheet-navigation.spec.js
@@ -1,1 +1,10 @@
 const { test, expect } = require('@playwright/test');
+
+const { chromium } = require('playwright');
+
+const browser = chromium.launch();
+const page = browser.newPage();
+
+// This is an empty test that does nothing
+
+browser.close();
