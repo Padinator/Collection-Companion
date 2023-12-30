bedbad0dbdf39623693bafd777fb4e6ac83ae34d
Dominik <sauterdominik1211@gmail.com>
2023-12-30 16:21:25 +0100

    Changed Playwright Test Config

--- tests/browser-test/playwright.config.js
+++ tests/browser-test/playwright.config.js
@@ -42,8 +42,7 @@ module.exports = defineConfig({
             name: 'chromium',
             use: {
                 ...devices['Desktop Chrome'],
-                headless: true,
-                storageState: 'playwright/.auth/user.json',
+                headless: true
             },
             dependencies: ['setup'],
         },
@@ -52,8 +51,7 @@ module.exports = defineConfig({
             name: 'firefox',
             use: {
                 ...devices['Desktop Firefox'],
-                headless: true,
-                storageState: 'playwright/.auth/user.json',
+                headless: true
             },
             dependencies: ['setup'],
         },
@@ -62,39 +60,10 @@ module.exports = defineConfig({
             name: 'webkit',
             use: {
                 ...devices['Desktop Safari'],
-                headless: true,
-                storageState: 'playwright/.auth/user.json',
+                headless: true
             },
             dependencies: ['setup'],
         },
-
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
     ],
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
 });
 
