a9b2f0cf2dc12c58d4bd24619d2a3ec1ddeac1a2
Dominik <sauterdominik1211@gmail.com>
2023-12-30 16:16:22 +0100

    Changed Playwright Test Config

--- tests/browser-test/tests/sheet-navigation.spec.js
+++ tests/browser-test/tests/sheet-navigation.spec.js
@@ -1,10 +1,5 @@
 const { test, expect } = require('@playwright/test');
 
-const { chromium } = require('playwright');
-
-const browser = chromium.launch();
-const page = browser.newPage();
-
-// This is an empty test that does nothing
-
-browser.close();
+test('Dummy Test', async ({ page }) => {
+    expect(true).toBe(true);
+});
