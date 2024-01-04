09faa01cc403e3eea9445fe1177c35a782e0db74
Dominik <46979046+Dominik308@users.noreply.github.com>
2024-01-04 12:59:29 +0100

    Update sheet-navigation.spec.js

--- tests/browser-test/tests/sheet-navigation.spec.js
+++ tests/browser-test/tests/sheet-navigation.spec.js
@@ -3,13 +3,13 @@ const { test, expect } = require('@playwright/test');
 test('Opening Main Page', async ({ page }) => {
 
     // Navigate to Main page
-    await page.goto('http://localhost');
+    await page.goto('http://localhost:8090');
 
     // Click on login button
     await page.click('#cc-login');
 
     // Check if on the I am on the next right page 
-    expect(page.url(), 'Not the right page ').toBe('http://localhost/mainpage.html');
+    expect(page.url(), 'Not the right page ').toBe('http://localhost:8090/mainpage.html');
 
     // // Click on first exercise sheet entry
     // await page.click('.fl-table tbody tr:first-child');
