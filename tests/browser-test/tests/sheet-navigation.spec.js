ac9b8f3ef18640fa5b945ba7a58f0472521f5da4
Tom Lauth <t-lauth@web.de>
2024-01-04 09:57:17 +0100

    first playwright test

--- tests/browser-test/tests/sheet-navigation.spec.js
+++ tests/browser-test/tests/sheet-navigation.spec.js
@@ -1,5 +1,21 @@
 const { test, expect } = require('@playwright/test');
 
-test('Dummy Test', async ({ page }) => {
-    expect(true).toBe(true);
+test('Opening Main Page', async ({ page }) => {
+
+    // Navigate to Main page
+    await page.goto('http://127.0.0.1:5500/Website/login.html');
+
+    // Click on login button
+    await page.click('#cc-login');
+
+    // Check if on the I am on the next right page 
+    expect(page.url(), 'Not the right page ').toBe('http://127.0.0.1:5500/Website/mainpage.html');
+
+    // // Click on first exercise sheet entry
+    // await page.click('.fl-table tbody tr:first-child');
+    // expect(page.url(), 'Not sheet url').toMatch(/^http:\/\/localhost\/uebungsblatt\/\d+/);
+
+    // // Does the back back button work?
+    // await page.click('.buttonDefault.zurueck');
+    // expect(page.url()).toBe('http://localhost/uebungsblatt/');
 });
