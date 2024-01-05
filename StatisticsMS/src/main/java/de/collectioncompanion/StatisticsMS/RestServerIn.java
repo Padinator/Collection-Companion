2c170ec7f998b039f535e4d51ce6c19f7f57ee70
KingSanta59 <Stefanheyn92@gmx.de>
2024-01-05 18:33:52 +0100

    Fixed errors for talking to microservices

--- StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/RestServerIn.java
+++ StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/RestServerIn.java
@@ -11,8 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestServerIn {
 
     @GetMapping
+    @CrossOrigin(exposedHeaders = {"Access-Control-Allow-Origin"})
     public ResponseEntity<String> foo() {
-        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("{ \"test\": \"123\" }");
+        String result = "{ \"test\": \"123\" }";
+        System.out.println(ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(result));
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(result);
+
+        /* // 2. variant
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "application/json");
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        responseHeaders.set("Access-Control-Allow-Methods", "GET,POST,PATCH,OPTIONS");
+        ResponseEntity<String> response = new ResponseEntity<String>(result, responseHeaders, HttpStatus.OK);
+        System.out.println(response);
+        return response;
+         */
     }
 
 }
--- StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/StatisticsMsApplication.java
+++ StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/StatisticsMsApplication.java
@@ -2,6 +2,9 @@ package de.collectioncompanion.StatisticsMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StatisticsMsApplication {
@@ -9,5 +12,16 @@ public class StatisticsMsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StatisticsMsApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+
 
 }
