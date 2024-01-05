0ea4b2a2d49a08115a086313cb8d06b1772b5124
Tom Lauth <t-lauth@web.de>
2024-01-05 15:24:26 +0100

    edit server file

--- StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/RestServerIn.java
+++ StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/RestServerIn.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8090")
 public class RestServerIn {
 
-    @CrossOrigin(origins = "http://localhost:8086")
     @GetMapping
     public ResponseEntity<String> foo() {
         return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("{ \"test\": \"123\" }");
