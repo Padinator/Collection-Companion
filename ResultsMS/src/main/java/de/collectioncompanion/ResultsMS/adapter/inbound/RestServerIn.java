0efcedd2610d37735ebecdcae121efc16893cd7a
KingSanta59 <Stefanheyn92@gmx.de>
2024-01-05 12:54:34 +0100

    Tests for fetching data in js

--- ResultsMS/src/main/java/de/collectioncompanion/ResultsMS/adapter/inbound/RestServerIn.java
+++ ResultsMS/src/main/java/de/collectioncompanion/ResultsMS/adapter/inbound/RestServerIn.java
@@ -2,10 +2,7 @@ package de.collectioncompanion.ResultsMS.adapter.inbound;
 
 import de.collectioncompanion.ResultsMS.data_files.ResultWaiterThread;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/collection")
@@ -28,6 +25,7 @@ public class RestServerIn {
         }
 
         System.out.println("Requested collection is: " + resultWaiterThread.getCollection().toString());
+        System.out.println("Response is: " + ResponseEntity.status(200).body(resultWaiterThread.getCollection().toString()));
 
         return ResponseEntity.status(200).body(resultWaiterThread.getCollection().toString());
     }
---
+++ StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/RestServerIn.java
@@ -0,0 +1,18 @@
+package de.collectioncompanion.StatisticsMS;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestServerIn {
+
+    @CrossOrigin(origins = "http://localhost:8086")
+    @GetMapping
+    public ResponseEntity<String> foo() {
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("{ \"test\": \"123\" }");
+    }
+
+}
