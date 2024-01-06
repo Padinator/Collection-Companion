413aa3e68ec7c5ab23251ee790ba57afeb4ee8aa
KingSanta59 <Stefanheyn92@gmx.de>
2024-01-06 15:18:58 +0100

    Created branch for unittests

--- ComposerMS/src/test/java/de/collectioncompanion/ComposerMS/ComposerMsApplicationTests.java
+++ ComposerMS/src/test/java/de/collectioncompanion/ComposerMS/ComposerMsApplicationTests.java
@@ -1,13 +1,30 @@
 package de.collectioncompanion.ComposerMS;
 
+import data_files.CollectionImpl;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ports.Collection;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 @SpringBootTest
 class ComposerMsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Nested
+	class CollectionTests {
+
+		@Test
+		void testInvalidTimeStamp1() {
+			Map<String, String> data = new TreeMap<>();
+			Collection collection = new CollectionImpl(data);
+		}
+
 	}
 
 }
