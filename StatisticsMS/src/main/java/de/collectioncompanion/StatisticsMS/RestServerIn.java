29fd2b230e02f4116b31c5eff7b7159c6abb406d
KingSanta59 <Stefanheyn92@gmx.de>
2024-01-06 10:40:35 +0100

    Fixed error requesting microservice

--- StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/RestServerIn.java
+++ StatisticsMS/src/main/java/de/collectioncompanion/StatisticsMS/RestServerIn.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8090")
 public class RestServerIn {
 
     @GetMapping
-    @CrossOrigin(exposedHeaders = {"Access-Control-Allow-Origin"})
+    @CrossOrigin(exposedHeaders = {"Access-Control-Allow-Origin", "Access-Control-Allow-Methods\", \"GET,POST,PATCH,OPTIONS"})
     public ResponseEntity<String> foo() {
         String result = "{ \"test\": \"123\" }";
         System.out.println(ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(result));
         return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(result);
 
-        /* // 2. variant
+        // 2. variant
+        /*
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("Content-Type", "application/json");
         responseHeaders.set("Access-Control-Allow-Origin", "*");
